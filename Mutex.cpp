607076187e7fe6e74d990a4176454fdac6f4d7d8
gettogetto <643274003@qq.com>
2016-07-29 22:42:14 +0800

    Mutex

---
+++ Mutex.cpp
@@ -0,0 +1,55 @@
+#include"Mutex.h"
+#include<assert.h>
+
+#define CHECK(ret) ({__typeof__(ret) err=(ret);\
+                    assert(err==0);})
+
+/*************************MutexLock***************************/
+MutexLock::MutexLock()
+    :is_locked(false)
+{
+    CHECK(pthread_mutex_init(&m_lock,NULL));
+
+}
+
+MutexLock::~MutexLock()
+{
+    //while(is_locked);
+    assert(is_locked==false);
+    //unlock();
+    pthread_mutex_destroy(&m_lock);//why CHECK the ret here will assert error?
+}
+
+void MutexLock::lock()
+{
+
+    CHECK(pthread_mutex_lock(&m_lock));
+    is_locked=true;
+}
+
+void MutexLock::unlock()
+{
+    is_locked=false;//do it before unlocking to avoid race condition
+    CHECK(pthread_mutex_unlock(&m_lock));
+}
+
+pthread_mutex_t* MutexLock::get_mutex_ptr()
+{
+    return &m_lock;
+}
+
+
+
+/*************************MutexLockGuard***************************/
+MutexLockGuard::MutexLockGuard(MutexLock& mutexlock)
+    :m_mutexlock(mutexlock)
+{
+    m_mutexlock.lock();
+}
+
+MutexLockGuard::~MutexLockGuard(){
+    m_mutexlock.unlock();
+}
+
+
+
---
+++ Mutex.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include<pthread.h>
+
+class MutexLock
+{
+public:
+    MutexLock();
+    ~MutexLock();
+    void lock();
+    void unlock();
+    pthread_mutex_t* get_mutex_ptr();
+
+    //nocopy
+    MutexLock(const MutexLock&)=delete;
+    MutexLock(MutexLock&&)=delete;
+    MutexLock& operator=(const MutexLock&)=delete;
+    MutexLock& operator=(MutexLock&&)=delete;
+private:
+    pthread_mutex_t m_lock;
+    volatile bool is_locked;
+    //todo :replace by "m_hoder" to indicate who is owning the lock
+};
+
+//a RAII class MutexLockGuard
+class MutexLockGuard{
+public:
+    explicit MutexLockGuard(MutexLock&);
+    ~MutexLockGuard();
+
+    MutexLockGuard(const MutexLockGuard&)=delete;
+    MutexLockGuard(MutexLockGuard&&)=delete;
+    MutexLockGuard& operator=(const MutexLockGuard&)=delete;
+    MutexLockGuard& operator=(MutexLockGuard&&)=delete;
+private:
+    MutexLock& m_mutexlock;//MutexLOck nocopyable,so use reference
+};

101cefbf0daeb69e506a765c167d20ac7981f777
gettogetto <643274003@qq.com>
2016-07-30 22:52:39 +0800

    Thread

---
+++ Thread.cpp
@@ -0,0 +1,71 @@
+#include"Thread.h"
+ThreadData::ThreadData(const ThreadFunc &func):
+    m_func(func)
+{
+
+}
+
+void ThreadData::run()
+{
+    m_func();
+}
+
+/*independent function*/
+void *startThread(void* arg)
+{
+    ThreadData* data=static_cast<ThreadData*>(arg);
+    data->run();
+    delete data;
+    return nullptr;
+}
+
+
+Thread::Thread(const ThreadFunc& func):
+    m_func(func),m_isRunning(false),m_joined(false),m_tid(0),m_data(nullptr)
+{
+
+}
+Thread::~Thread()
+{
+    if(m_isRunning&&!m_joined){
+        pthread_detach(m_tid);
+    }
+
+   // delete m_data;
+    //m_data=nullptr;
+
+}
+
+int Thread::run()
+{
+    assert(!m_isRunning);
+    m_isRunning=true;
+    m_data=new ThreadData(m_func);
+    if(pthread_create(&m_tid,NULL,startThread,static_cast<void*>(m_data))==0){
+        return 0;
+    }else{
+        m_isRunning=false;
+        delete m_data;
+        return -1;
+    }
+
+}
+
+int Thread::join()
+{
+    assert(m_isRunning);
+    assert(!m_joined);
+    m_joined=true;
+    return pthread_join(m_tid,NULL);
+}
+
+bool Thread::isRunning()
+{
+    return m_isRunning;
+}
+
+pthread_t Thread::getTid()
+{
+    return m_tid;
+}
+
---
+++ Thread.h
@@ -0,0 +1,42 @@
+#pragma once
+#include<pthread.h>
+#include<functional>
+#include<assert.h>
+class ThreadData{
+public:
+    typedef std::function<void()> ThreadFunc;
+    explicit ThreadData(const ThreadFunc &func);
+    void run();
+private:
+    ThreadFunc m_func;
+};
+
+
+
+
+
+class Thread{
+public:
+    typedef std::function<void()> ThreadFunc;
+    explicit Thread(const ThreadFunc& func);
+    ~Thread();
+
+    //Thread(const Thread&)=delete;
+    //Thread(Thread&&)=delete;
+    //Thread& operator=(const Thread&)=delete;
+    //Thread& operator=(Thread&&)=delete;
+
+    int run();
+    int join();
+    bool isRunning();
+    pthread_t getTid();
+
+
+private:
+
+    ThreadFunc m_func;
+    bool m_isRunning;
+    bool m_joined;
+    pthread_t m_tid;
+    ThreadData *m_data;
+};

6bd67c7e2a8ce7b89ddc40e8e4ef75cf1e3ad946
gettogetto <643274003@qq.com>
2016-09-15 16:59:11 +0800

    test

--- test/Threadpool_test.cpp
+++ test/Threadpool_test.cpp
@@ -7,33 +7,24 @@
 using namespace std;
 const int MAX_TASKS=50;
 void hello(void* arg){
-    int* x=(int*)arg;
     int i=0;
     while(i++<=100000);
-    cout<<"calculate finished "<<*x<<std::endl;
+    cout<<"hello "<<rand()%100<<endl;
+
 }
 
 int main(int argc, char *argv[])
 {
     ThreadPool tp(2);
-    int ret=tp.initialize_threadpool();
-    if(ret==-1){
-        cerr<<"in main() failed to initialize threadpool\n";
-        return 0;
-    }
-    for(int i=0;i<MAX_TASKS;i++){
-        int* x=new int();
-        *x=i+1;
 
-        Task *t=new Task(std::bind(&hello,static_cast<void*>(x)));
-        cout<<"in main() adding to pool,task "<<i+1<<endl;
-        tp.add_task(t);
+    for(int i=0;i<MAX_TASKS;i++){
+
+        Task t(std::bind(&hello,nullptr));
+        cout<<"in main() adding to pool,task "<<i<<endl;
+        tp.addTask(t);
     }
     sleep(2);
-    tp.destroy_threadpool();
-    cout<<"in main() all finished\n";
-
-
+    //tp.destroy();
     return 0;
 }
 
